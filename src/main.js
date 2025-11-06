a9d7b813db2f547a5c553837caa8b3092e0a40a6
Alla-Nazarenko <umnyj.start@gmail.com>
2025-11-06 13:33:28 +0100

    partials + base settings

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/furniture-list.js';
+import './js/about-us.js';
+import './js/popular-products.js';
+import './js/faq.js';
+import './js/feedbacks.js';
+
+import './js/furniture-details-modal.js';
+import './js/order-modal.js';
+
+import './js/footer.js';
