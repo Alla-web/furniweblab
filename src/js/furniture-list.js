62d3ed81a63b2ca0aa3ea735f3124cafedd9ffdf
Alla-Nazarenko <umnyj.start@gmail.com>
2025-11-07 14:30:22 +0100

    separate styles for izitoast

--- src/js/furniture-list.js
+++ src/js/furniture-list.js
@@ -1,1 +1,27 @@
+import axios from 'axios';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+axios.defaults.baseURL = 'https://furniture-store-v2.b.goit.study/api';
+
+async function fetchCategories() {
+  try {
+    const { data: categories } = await axios('/categories');
+    return categories;
+  } catch (error) {
+    iziToast.error({
+      message: `${error.message ?? String(err)}`,
+      position: 'topCenter',
+      timeout: 3000,
+      backgroundColor: '#EF4040',
+      messageColor: 'white',
+      close: false,
+    });
+  }
+}
+
+fetchCategories();
+
+async function renderCategories(categories) {}
+//додати кольори для повідомлень изитост у стилі
